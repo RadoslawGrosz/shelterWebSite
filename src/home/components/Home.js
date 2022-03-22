fe102b6218dd34b5d0777c250e9383deffc1ff17
Radoslaw Grosz <r.grosz@i4b.pl>
2022-03-22 18:53:48 +0100

    Try to improve performance

--- src/home/components/Home.js
+++ src/home/components/Home.js
@@ -1,11 +1,12 @@
 import React, { useRef, Suspense } from 'react';
-import Header from './header/Header';
+// import Header from './header/Header';
 import Menu from './header/Menu';
 import AdoptInfoSection from './adoptInfoSection/AdoptInfoSection';
 // import MainSection from './mainSection/MainSection';
 import Footer from './Footer';
 import StyledWrapper from '../styles/StyledHome';
 
+const Header = React.lazy(() => import('./header/Header'));
 const MainSection = React.lazy(() => import('./mainSection/MainSection'));
 
 function Home() {
